5217495480ce512f408a6992fa0dd42ed8a31985
Arun G Jose <arunjose2004@gmail.com>
2026-02-09 14:53:15 +0530

    Simple file saving program

---
+++ code/filesave.py
@@ -0,0 +1,16 @@
+import sounddevice as sd
+import soundfile as sf
+
+fs=44100
+seconds=5
+rec_file='../files/soundtest2.wav'
+
+print(f"Recording started for {seconds} seconds!!")
+my_recording=sd.rec(int(seconds*fs), samplerate=fs, channels=1)
+
+sd.wait()
+
+print("Recording has ended!!")
+sf.write(rec_file, my_recording, fs)
+
+print("Recording is saved!")
